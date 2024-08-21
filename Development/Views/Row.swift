08aaea53af69eb739509fff0b18caf1b85c3997b
Pedro Almeida <ip4dro@gmail.com>
2024-08-21 17:58:47 +0200

    improve custom picker accessiblity

--- Development/Views/Row.swift
+++ Development/Views/Row.swift
@@ -25,29 +25,40 @@ struct InlineRow: View {
     @Environment(\.labelTransformation)
     private var labelTransformation
 
+    private var picker: some View {
+        Picker(data.title, selection: data.$selection) {
+            ForEach(data.options) { option in
+                Text(option.label)
+            }
+        }
+    }
+
     var body: some View {
         #if VERBOSE
         Self._printChanges()
         #endif
-        return HStack {
-            Text(verbatim: data.title)
-            Menu {
-                Picker(data.title, selection: data.$selection) {
-                    ForEach(data.options) { option in
-                        Text(option.label)
-                    }
-                }
-            } label: {
-                HStack {
+        return Menu {
+            picker
+        } label: {
+            HStack {
+                Text(verbatim: data.title).layoutPriority(1)
+                Group {
                     Text(verbatim: labelTransformation.apply(to: data.selection))
+                        .frame(maxWidth: .infinity, alignment: .trailing)
                     Image(systemName: "chevron.up.chevron.down")
                 }
                 .opacity(0.5)
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                .padding(.vertical, 8)
             }
+            .padding(.vertical, 8)
         }
         .foregroundStyle(.foreground)
-        .safeAreaInset(edge: .bottom, spacing: .zero, content: Divider.init)
+        .safeAreaInset(
+            edge: .bottom,
+            spacing: .zero,
+            content: Divider.init
+        )
+        .accessibilityRepresentation {
+            picker
+        }
     }
 }
