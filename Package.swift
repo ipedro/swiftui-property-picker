0c07de271b800c25a393c96ceb80bd4df96c11c4
Pedro Almeida <ip4dro@gmail.com>
2024-09-16 22:40:10 +0200

    streamline package manifest

--- Package.swift
+++ Package.swift
@@ -1,54 +1,8 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 
 import PackageDescription
 
-let isDevelopment = !Context.packageDirectory.contains(".build/checkouts/") && !Context.packageDirectory.contains("SourcePackages/checkouts/")
-
-var targets: [Target] = [.target(
-        name: "PropertyPicker-Examples",
-        dependencies: ["PropertyPicker"],
-        path: "Examples"
-)]
-
-if isDevelopment {
-    targets.append(
-        .target(
-            name: "PropertyPicker",
-            path: "Development",
-            swiftSettings: [.define("VERBOSE")],
-            plugins: [
-                .plugin(
-                    name: "SwiftLintBuildToolPlugin",
-                    package: "SwiftLintPlugins"
-                )
-            ]
-        )
-    )
-} else {
-    targets.append(
-        .target(
-            name: "PropertyPicker",
-            path: ".",
-            sources: ["PropertyPicker.swift"]
-        )
-    )
-}
-
-var dependencies = [Package.Dependency]()
-if isDevelopment {
-    dependencies.append(
-        .package(
-            url: "https://github.com/SimplyDanny/SwiftLintPlugins",
-            from: "0.55.1"
-        )
-    )
-    dependencies.append(
-        .package(
-            url: "https://github.com/nicklockwood/SwiftFormat",
-            from: "0.54.0"
-        )
-    )
-}
+let isDevelopment = !Context.packageDirectory.contains("/checkouts/")
 
 let package = Package(
     name: "swiftui-property-picker",
@@ -65,7 +19,36 @@ let package = Package(
             name: "PropertyPicker-Examples",
             targets: ["PropertyPicker-Examples"]
         )
-    ], 
-    dependencies: dependencies,
-    targets: targets
+    ],
+    dependencies: isDevelopment ? [
+        .package(
+            url: "https://github.com/SimplyDanny/SwiftLintPlugins",
+            from: "0.55.1"
+        ),
+        .package(
+            url: "https://github.com/nicklockwood/SwiftFormat",
+            from: "0.54.0"
+        )
+    ] : nil,
+    targets: [
+        .target(
+            name: "PropertyPicker-Examples",
+            dependencies: ["PropertyPicker"],
+            path: "Examples"
+        ),
+        .target(
+            name: "PropertyPicker",
+            path: isDevelopment ? "Development" : ".",
+            sources: isDevelopment ? nil : ["PropertyPicker.swift"],
+            swiftSettings: isDevelopment ? [
+                .define("VERBOSE")
+            ] : nil,
+            plugins: isDevelopment ? [
+                .plugin(
+                    name: "SwiftLintBuildToolPlugin",
+                    package: "SwiftLintPlugins"
+                )
+            ] : nil
+        )
+    ]
 )
