9b9c50af2b12affe91f57122d9e4ce4ac810381b
Pedro Almeida <ip4dro@gmail.com>
2024-11-16 07:26:35 +0100

    improve propertyID

--- Development/Models/PropertyID.swift
+++ Development/Models/PropertyID.swift
@@ -3,36 +3,14 @@ import Foundation
 /// `PropertyID` provides a unique identifier for property picker elements,
 /// facilitating the tracking and management of property picker states and configurations
 /// across different components of an application.
-///
-/// It utilizes `ObjectIdentifier` under the hood to guarantee uniqueness, basing the identity
-/// on the type information of `PropertyPickerKey` conforming types. This ensures that each
-/// property picker type is associated with a distinct identifier, preventing conflicts and
-/// improving traceability in systems that manage multiple types of property pickers.
-///
-/// Usage of this ID is crucial in scenarios where properties need to be dynamically
-/// managed and accessed across various UI components or data handling layers.
-public struct PropertyID: Hashable {
-    public static func == (lhs: PropertyID, rhs: PropertyID) -> Bool {
-        lhs.value == rhs.value
+public struct PropertyID: Hashable, CustomDebugStringConvertible {
+    public var metadata: UnsafeRawPointer
+
+    public init<K: PropertyPickerKey>(_: K.Type = K.self) {
+        self.metadata = unsafeBitCast(K.self, to: UnsafeRawPointer.self)
     }
 
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(value)
-    }
-
-    let type: Any.Type
-
-    /// The underlying value storing the unique identifier based on type information.
-    private let value: ObjectIdentifier
-
-    /// Initializes a new identifier for a property picker key.
-    /// The identifier is derived from the type of the `PropertyPickerKey` conforming type,
-    /// ensuring that each key type has a unique identifier.
-    ///
-    /// - Parameter key: The type of the property picker key. The default value `K.self`
-    ///   captures the caller's type context, automatically providing type-specific uniqueness.
-    init<K: PropertyPickerKey>(_ key: K.Type = K.self) {
-        value = ObjectIdentifier(key)
-        type = key
+    public var debugDescription: String {
+        _typeName(unsafeBitCast(metadata, to: Any.Type.self), qualified: false)
     }
 }
